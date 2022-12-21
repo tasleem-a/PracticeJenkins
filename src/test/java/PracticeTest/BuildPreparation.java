accb289a05c67037c3ac033faebe2e5a1dd01e98
tasleem-a <tasleem.29a@gmail.com>
2022-12-21 12:14:37 +0530

    Third Commit Changes

---
+++ src/test/java/PracticeTest/BuildPreparation.java
@@ -0,0 +1,12 @@
+package PracticeTest;
+
+import org.testng.annotations.Test;
+
+public class BuildPreparation {
+	@Test
+	public void test()
+	{
+		System.out.println("Build is preparing............****");
+	}
+
+}
